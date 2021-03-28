e75d554ff721da345de0d62595137173f5deba90
FiLl0s0FeAr <vivasu5@ukr.net>
2021-03-28 17:28:28 +0300

    add DTO single user (api) and tests get query

---
+++ src/test/java/API/Queries/TestsGET.java
@@ -0,0 +1,30 @@
+package API.Queries;
+
+import API.DTO.SignleUser;
+import jdk.jfr.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestsGET {
+
+    @Test
+    @Description("get information about single user")
+    public void getSingleUser(){
+        SignleUser responseSingleUser = given().get("https://reqres.in/api/users/2").
+                then().assertThat().statusCode(200).
+                and().extract().as(SignleUser.class);
+
+        Assert.assertEquals(responseSingleUser.getData().get("id").asInt(), 2);
+        Assert.assertEquals(responseSingleUser.getData().get("email").asText(), "janet.weaver@reqres.in");
+        Assert.assertEquals(responseSingleUser.getSupport().get("text").asText(), "To keep ReqRes free, contributions towards server costs are appreciated!");
+    }
+
+    @Test
+    @Description("singe user not found")
+    public void getSingleUserNotFound(){
+        given().when().get("https://reqres.in/api/users/23").
+                then().assertThat().statusCode(404);
+    }
+}
